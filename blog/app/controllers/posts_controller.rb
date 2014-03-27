cf6c8e436ad679ec43ed0aebbe27929d8abd0b5b
Liakh Ulia <sun_in_july@mail.ru>
2014-03-27 18:55:17 +0300

    simple web-app

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+def new
+  @post = Post.new
+end
+ 
+ def create
+    @post = Post.new(post_params)
+ 
+   if @post.save
+        redirect_to @post
+    else 
+        render 'new'
+  end
+  end 
+ 
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
+  def show
+  @post = Post.find(params[:id])
+end
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
+http_basic_authenticate_with name: "guest", password: "secret", except: [:index, :show]
+def index
+  @posts = Post.all
+end
+def edit
+  @post = Post.find(params[:id])
+end
+def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
+
+end
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'b5577b14bd271f14028fce9fd8582f72e223f8d5e697105b5c5c61aa631ea4b5fd267cfde2b43be8c49f0a8e6e5fcae40d32cdd91ad3b54c7f19d59a927a9a4b'
